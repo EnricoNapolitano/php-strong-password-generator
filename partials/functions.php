b7334a7f50c5252ee3037ee473527c368907d2cc
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-08 21:08:27 +0100

    code correction

--- partials/functions.php
+++ partials/functions.php
@@ -3,13 +3,10 @@
 //function random password
 function get_random_pass($n){
 
-    //lenght comes from user
-    $pass_length = $n;
-
     if(is_numeric($n) && $n >= 8 && $n <= 15) { //input validation
         $password = '';
 
-        for($i = 0; $i < $pass_length; $i++) {
+        for($i = 0; $i < $n; $i++) {
             $password .= chr(rand(65, 125)); //chr is a function to get ASCII characters
         }
     return $password;
