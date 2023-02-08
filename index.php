23dec5eeab5396a2517229e72bce2ac57eaf9caa
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-08 21:38:22 +0100

    added radio checks (layout)

--- index.php
+++ index.php
@@ -27,12 +27,39 @@ if($length){
         </header>
 
         <!-- form -->
-        <form action="#" method="GET" class="d-flex justify-content-between align-items-center">
-            <label for="length">Lunghezza password:</label>
-            <div class="d-flex align-items-center">
+        <form action="#" method="GET">
+
+            <!-- password's length -->
+            <div class="d-flex justify-content-between align-item-center">
+                <label for="length" class="d-block">Lunghezza password:</label>
                 <input class="input-group-text" id="length" name="length" type="number" min="8" max="15" step="1">
-                <button class="btn btn-primary ms-2">INVIA</button>
             </div>
+
+            <!-- radio checkers -->
+            <div class="d-flex justify-content-between align-item-center pt-3">
+                <span>Consentire ripetizione caratteri:</span>
+                <div class="d-flex">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="repeat" id="yes_repeat">
+                        <label class="form-check-label pe-3" for="yes_repeat">
+                            Si
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="repeat" id="no_repeat" checked>
+                        <label class="form-check-label" for="no_repeat">
+                            No
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+            <!-- button -->
+            <div class="d-flex justify-content-end">
+                <button class="btn btn-primary mt-4">INVIA</button>
+            </div>
+
+            
         </form>
 
     </div>
