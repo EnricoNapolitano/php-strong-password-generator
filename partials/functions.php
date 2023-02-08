07d2d26387cf14ec1ccc902ad0d7c955e19e17ef
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-08 22:50:07 +0100

    lil change to function code

--- partials/functions.php
+++ partials/functions.php
@@ -7,28 +7,27 @@ function get_random_pass($n, $r){
     if(!is_numeric($n) || $n <= 8 && $n >= 15) { 
         return 'Non hai inserito un valore valido';
     } else {
-
+        $password = '';
+        
         // condition where it is possible to repeat characters
         if($r === 'si') {
-            $password = '';
                 for($i = 0; $i < $n; $i++) {
-                $password .= chr(rand(65, 125)); //chr: function to get ASCII characters
+                $password .= chr(rand(65, 80)); //chr: function to get ASCII characters
                 }
-            return $password;
 
         // instead here it is if it's not possible to repeat characters
         } else {
-            $password = '';
             $i = 0;
             while ($i < $n) {
-                $chr = chr(rand(65, 125));
+                $chr = chr(rand(65, 80));
                 if (!str_contains($password, $chr)) {
                     $password .= $chr;
                     $i++;
                 }
             }
-            return $password;
         }
+
+        return $password;
     }
 }
 
