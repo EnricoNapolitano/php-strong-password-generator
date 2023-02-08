15cb085c7d4b2c1328c21346492d3666607d1607
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-08 17:39:36 +0100

    lil change to main function (range of characters)

--- partials/functions.php
+++ partials/functions.php
@@ -6,9 +6,10 @@ function get_random_pass($n){
     //lenght comes from user
     $pass_length = $n;
     $password = '';
+
     for($i = 0; $i < $pass_length; $i++) {
-    $password .= chr(rand(32, 126));
-  }
+        $password .= chr(rand(65, 125)); //chr is a function to get ASCII characters
+    }
   return $password;
 }
 
