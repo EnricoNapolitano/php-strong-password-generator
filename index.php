85b6ddf332334dd769a1ac03d4980607d9bd95d9
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-08 16:56:17 +0100

    created new file partial: head.php

--- index.php
+++ index.php
@@ -14,23 +14,10 @@ if($length){
 
 <!DOCTYPE html>
 <html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gneratore password</title>
-    <!-- bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
-    <style>
-        .wrap {
-            max-width: 800px;
-        }
-    </style>
-</head>
+<?php include 'partials/head.php' /***HEAD***/ ?>
 
 <body>
-
     <div class="container wrap pt-5">
 
         <!-- header -->
@@ -49,6 +36,5 @@ if($length){
         </form>
 
     </div>
-    
 </body>
 </html>
--- password_page.php
+++ password_page.php
@@ -10,18 +10,13 @@ include 'partials/functions.php';
 
 <!DOCTYPE html>
 <html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gneratore password</title>
-    <!-- bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-</head>
+
+<?php include 'partials/head.php' /***HEAD***/ ?>
 
 <!-- alert -->
 <div class=" container alert alert-primary mt-5"> 
     <b class="pe-4">La tua password è: </b> 
      <?php echo get_random_pass($_SESSION['length']) ?>
 </div>
+
 </html>
